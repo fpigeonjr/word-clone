b61108e94ea28ec7405d6414c0a2d55a363ec37e
fpigeonjr <frank.pigeonjr@gmail.com>
2023-08-27 20:03:21 -0500

    adds styles for guesses

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,18 +1,23 @@
+import { range } from "../../utils"
 import { checkGuess } from "../../game-helpers"
 
 const Guess = ({ value, answer }) => {
   const checkedGuess = checkGuess(value, answer)
   console.log({ checkedGuess })
 
+  const Cell = ({ letter, status }) => {
+    const className = status ? `cell ${status}` : "cell"
+    return <span className={className}>{letter}</span>
+  }
+
   return (
     <p className="guess">
-      {checkedGuess?.map((guess, index) => (
-        <span
-          key={index}
-          className={`cell ${guess.status}`}
-        >
-          {guess.letter}
-        </span>
+      {range(5).map((num) => (
+        <Cell
+          key={num}
+          letter={checkedGuess ? checkedGuess[num].letter : undefined}
+          status={checkedGuess ? checkedGuess[num].status : undefined}
+        />
       ))}
     </p>
   )
